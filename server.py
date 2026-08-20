8cd2598849872b0d86da55b5cbe2d26e24f75008
BakhshiZ <bakhshizulfiqarr@gmail.com>
2026-08-20 13:55:30 +0200

    Added logic for connection manager and implemented shared canvas

--- server.py
+++ server.py
@@ -1,20 +1,41 @@
-from fastapi import FastAPI, WebSocket
+from fastapi import FastAPI, WebSocket, WebSocketDisconnect
 from fastapi.responses import HTMLResponse
 import os
 
-app = FastAPI()
+class ConnectionManager:
+    def __init__(self):
+        self.current_connections: list[WebSocket] = []
 
-@app.get('/')
-async def home_page():
-    return HTMLResponse(os.path.join("static", "home-page.html"))
+    async def add_user(self, websocket: WebSocket):
+        await websocket.accept()
+        self.current_connections.append(websocket)
+
+    def remove_user(self, websocket: WebSocket):
+        self.current_connections.remove(websocket)
+
+    async def broadcast_message(self, message):
+        for connection in self.current_connections:
+            await connection.send_text(message)
+
+app = FastAPI()
+connection_manager = ConnectionManager()
+
+@app.get("/")
+async def main_page():
+    return HTMLResponse(os.path.join("static", "canvas_page.html"))
 
 @app.websocket("/ws")
 async def websocket_endpoint(websocket: WebSocket):
-    await websocket.accept()
+    await connection_manager.add_user(websocket)
 
-    while True:
-        data = await websocket.receive_text()
-        print(data)
+    try:
+        while True:
+            data = await websocket.receive_text()
+            mouseX, mouseY = data.split(',')
+            await connection_manager.broadcast_message(f"{mouseX},{mouseY}")
+
+    except WebSocketDisconnect:
+        await connection_manager.broadcast_message("User has disconnected")
 
 if __name__ == '__main__':
     import uvicorn
